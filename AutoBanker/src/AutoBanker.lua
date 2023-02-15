c4a4f3cedea2e866ada40219023e36a1103b7d9d
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 19:33:24 +0000

    Now moves items to the bank if the item in the bank is not a full stack

--- AutoBanker/src/AutoBanker.lua
+++ AutoBanker/src/AutoBanker.lua
@@ -3,6 +3,7 @@ AutoBanker = {}
 AutoBanker.name = "AutoBanker"
 
 function AutoBanker.Initialize()
+    EVENT_MANAGER:RegisterForEvent(AutoBanker.name, EVENT_OPEN_BANK, AutoBanker.OnBankOpened)
 end
 
 function AutoBanker.OnAddOnLoaded(event, addonName)
@@ -12,4 +13,26 @@ function AutoBanker.OnAddOnLoaded(event, addonName)
     end
 end
 
+function AutoBanker.OnBankOpened(event, bankBag)
+    if bankBag == BAG_BANK then
+        -- Get the data of both the player bag and their bank
+        PlayerInventory = SHARED_INVENTORY:GenerateFullSlotData(nil,BAG_BACKPACK)
+        PlayerBank = SHARED_INVENTORY:GenerateFullSlotData(nil,BAG_BANK)
+
+        -- for each item in the player bag, search for that same item in the player's bank
+        for _, itemInBag in pairs(PlayerInventory) do
+            for _, itemInBank in pairs(PlayerBank) do
+                if (itemInBag.name == itemInBank.name) then
+                    d("Found "..itemInBag.name.." in bank")
+                    if IsProtectedFunction("RequestMoveItem") then
+                        CallSecureProtected("RequestMoveItem", BAG_BACKPACK, itemInBag.slotIndex, BAG_BANK, itemInBank.slotIndex, 200)
+                    end
+                end
+            end
+        end
+    end
+end
+
+
+-- Must keep at the end of the file, else it errors on load
 EVENT_MANAGER:RegisterForEvent(AutoBanker.name, EVENT_ADD_ON_LOADED, AutoBanker.OnAddOnLoaded)
