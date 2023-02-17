e40dbab0b56e5a9db3672cfa60d127722f518f06
Kieran <kieran.winfield464@gmail.com>
2023-02-17 21:37:49 +0000

    Added a message that tells you how many items were found then moved to the bank

--- AutoBanker/src/AutoBanker.lua
+++ AutoBanker/src/AutoBanker.lua
@@ -2,13 +2,9 @@ AutoBanker = {}
 
 AutoBanker.name = "AutoBanker"
 
-function AutoBanker.Initialize()
-    EVENT_MANAGER:RegisterForEvent(AutoBanker.name, EVENT_OPEN_BANK, AutoBanker.OnBankOpened)
-end
-
 function AutoBanker.OnAddOnLoaded(event, addonName)
     if addonName == AutoBanker.name then
-        AutoBanker.Initialize()
+        EVENT_MANAGER:RegisterForEvent(AutoBanker.name, EVENT_OPEN_BANK, AutoBanker.OnBankOpened)
         EVENT_MANAGER:UnregisterForEvent(AutoBanker.name, EVENT_ADD_ON_LOADED)
     end
 end
@@ -18,6 +14,7 @@ function AutoBanker.OnBankOpened(event, bankBag)
         -- Get the data of both the player bag and their bank
         PlayerInventory = SHARED_INVENTORY:GenerateFullSlotData(nil,BAG_BACKPACK)
         PlayerBank = SHARED_INVENTORY:GenerateFullSlotData(nil,BAG_BANK)
+        local itemCount = 0;
 
         -- for each item in the player bag, search for that same item in the player's bank
         for _, itemInBag in pairs(PlayerInventory) do
@@ -25,11 +22,14 @@ function AutoBanker.OnBankOpened(event, bankBag)
                 if (itemInBag.name == itemInBank.name) then
                     if IsProtectedFunction("RequestMoveItem") then
                         CallSecureProtected("RequestMoveItem", BAG_BACKPACK, itemInBag.slotIndex, BAG_BANK, itemInBank.slotIndex, 200)
+                        itemCount = itemCount + 1
                     end
                 end
             end
         end
     end
+
+    d("Attempted to move"..itemCount.." items to your bank")
 end
 
 
